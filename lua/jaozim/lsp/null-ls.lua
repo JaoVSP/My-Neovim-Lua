535dbedfbe18651c303bde495d68615c947bba5f
João Vitor <joao.prueza@gmail.com>
2022-09-15 15:29:40 -0300

    Update null-ls.lua

--- lua/jaozim/lsp/null-ls.lua
+++ lua/jaozim/lsp/null-ls.lua
@@ -1,20 +1,18 @@
-local null_ls = require("null-ls")
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+  return
+end
 
 local formatting = null_ls.builtins.formatting
 
-null_ls.setup {
-  sources = {
-	formatting.eslint,
-	formatting.prettier,
-  },
-  on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup_format,
-        buffer = 0,
-        callback = function() vim.lsp.buf.formatting_seq_sync() end
-      })
-    end
-  end,
-}
+local diagnostics = null_ls.builtins.diagnostics
+
+
+null_ls.setup({
+    sources = {
+      formatting.prettier.with({ extra_args = { '--single-quote' }}),
+      formatting.black.with({ extra_args = { '--fast' }}),
+      diagnostics.eslint_d,
+      diagnostics.flake8,
+    },
+})
