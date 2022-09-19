e8abb38cb9d3ae853ce7b1be70c1bc72ff2231f5
João Vitor <joao.prueza@gmail.com>
2022-09-19 11:35:59 -0300

    Update colorscheme.lua

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,6 +1,13 @@
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+require("tokyonight").setup({
+  style = "night",
+  styles = {
+    functions = {}
+  },
+  sidebars = { "qf", "vista_kind", "terminal", "packer" },
+  on_colors = function(colors)
+    colors.hint = colors.orange
+    colors.error = "#ff0000"
+  end
+})
 
 vim.cmd[[colorscheme tokyonight]]
