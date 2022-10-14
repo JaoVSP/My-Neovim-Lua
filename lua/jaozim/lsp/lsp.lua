777c8dc899b907f9bf7a0b35df76f3eb1cc13b5e
João Vitor <joao.prueza@gmail.com>
2022-10-14 14:56:53 -0300

    Update lsp.lua

--- lua/jaozim/lsp/lsp.lua
+++ lua/jaozim/lsp/lsp.lua
@@ -1,3 +1,30 @@
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.rust_analyzer.setup{}
+require('lspconfig')['pyright'].setup{
+  cmd = cmd,
+  filetypes = { 'python' },  
+  on_attach = on_attach, 
+}
+
+require('lspconfig')['tsserver'].setup{
+  on_attach = on_attach,
+  filetypes = {  
+      'javascript',
+      'javascriptreact',
+      'javascript.jsx',
+      'typescript',
+      'typescriptreact',
+      'typescript.tsx',
+  },
+  
+  cmd = { "typescript-language-server", "--stdio" },
+  capabilities = capabilities
+}
+
+require('lspconfig')['rust_analyzer'].setup{
+  cmd = { 'rust-analyzer' },
+  filetypes = { 'rust' },  
+  on_attach = on_attach,
+
+  settings = {
+    ["rust-analyzer"] = {}
+  }
+}
