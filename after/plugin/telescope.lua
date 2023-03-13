9b6545f0c65ba6f05de961e3bb0cdd5e5dd3347e
João Vitor <joao.prueza@gmail.com>
2023-03-13 16:29:49 -0300

    change(shortcut)

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,4 +4,4 @@ local cmd = vim.cmd
 
 set("n", "<Leader><Leader>", ":Telescope find_files<CR>", ns)
 set("n", "<C-p>", ":Telescope buffers<CR>", ns)
-set("n", "<C-/>", ":Telescope current_buffer_fuzzy_find<CR>", ns)
+set("n", "<C-l>", ":Telescope current_buffer_fuzzy_find<CR>", ns)
