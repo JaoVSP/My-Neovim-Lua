07a756c44d32702bbdbb09051069e1b499841a69
João Vitor <joao.prueza@gmail.com>
2023-05-26 16:18:08 -0300

    keymap blame_line

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,1 +1,15 @@
-require('gitsigns').setup()
+require('gitsigns').setup{
+    on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+     -- Actions
+      map("n", "<C-g>", gs.blame_line)
+
+    end
+}
