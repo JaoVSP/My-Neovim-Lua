3523a786b2258e1126b5edf73a32f3608e936be3
Jaozim420 <joao.prueza@gmail.com>
2023-05-26 16:31:51 -0300

    add c & cpp in ensure installed

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "javascript", "lua", "python", "typescript", "tsx", "rust", "prisma" },
+  ensure_installed = { "javascript", "lua", "python", "typescript", "tsx", "rust","cpp", "c" ,"prisma" },
   sync_install = false,
   
   highlight = {
