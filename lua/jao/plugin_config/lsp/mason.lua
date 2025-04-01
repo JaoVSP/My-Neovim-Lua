912593fdccd4b912ee7c4efdb4d5972aa31d30b5
João Vitor <joao.soarespru@gmail.com>
2025-04-01 19:10:00 -0300

    Update mason.lua

--- lua/jao/plugin_config/lsp/mason.lua
+++ lua/jao/plugin_config/lsp/mason.lua
@@ -1,4 +1,4 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "pyright", "tsserver", "rust_analyzer", "clangd", "eslint" },
+	ensure_installed = { "pyright", "ts_ls", "rust_analyzer", "clangd", "eslint" },
 })
